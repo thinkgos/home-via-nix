a5c60e97bd2918435989b2c450a32192eead12e2
thinkgo <thinkgo@aliyun.com>
2025-12-07 07:09:03 +0800

    Init project

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Home Manager configuration of thinkgo";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, home-manager, ... }: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { pkgs, ... }: {
+        legacyPackages.homeConfigurations.thinkgo = inputs.home-manager.lib.homeManagerConfiguration{
+          inherit pkgs;
+
+          modules = [
+            ./home.nix
+          ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      
+        devShells.default = pkgs.mkShell {
+          packages = [
+          ];
+          env = {
+          };
+          # SHELL = "${pkgs.zsh}/bin/zsh";
+          shellHook = ''
+            echo "development shell ready!"
+          '';
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,116 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "thinkgo";
+  home.homeDirectory = "/home/thinkgo";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs;[
+    #! 基础工具
+    tmux        # 终端多路复用工具
+    vim         # 终端文本编辑器  
+    jq          # json美化过滤
+
+    #! 命令行替代工具
+    bat         # 替代cat
+    eza         # 替代ls
+    ripgrep     # 替代grep
+    fd          # 替代find
+    dust        # 替代du  
+    zoxide      # 替代z, 快速跳转目录
+
+    #! 终端工具
+    gitui       # git ui界面
+    delta       # git-delta, git语法高亮分页器
+    bottom      # 图形化进程/系统监控器
+
+    #! 通用工具
+    yazi        # 终端文件管理
+    zellij      # 终端多路复用工具
+    atuin       # 命令历史记录
+    gfold       # 跟踪git仓库
+    starship    # 提示工具
+    tealdeer    # 快速查找命令, 同tldr
+    tokei       # 统计代码行数
+    glow        # markdown预览器
+
+    #! 编程相关
+    sccache     # 缓存编译结果
+    mdbook      # 从markdown文档生成book
+
+    #! 环境管理
+    direnv      # shell扩展用于管理环境变量
+    mise        # 管理开发环境, 软件多版本管理
+
+
+    #! 编辑器
+
+
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/thinkgo/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
