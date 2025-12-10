19553d78d344d1b334f49479d4c71b9a3505e4f7
thinkgo <thinkgo@aliyun.com>
2025-12-11 06:47:32 +0800

    feat: add nc

--- home/default.nix
+++ home/default.nix
@@ -31,14 +31,15 @@
   # environment.
   home.packages = with pkgs;[
     #! 基础工具
-    tree        # 目录树
-    nmap        # 网络扫描工具
-    curl        # 网络请求工具
-    wget        # 网络下载工具
-    tmux        # 终端多路复用工具
-    jq          # json美化过滤
-    yq          # yaml美化过滤
-    fastfetch   # 系统信息查看工具
+    tree            # 目录树
+    nmap            # 网络扫描工具
+    netcat-openbsd  # nc 网络工具
+    curl            # 网络请求工具
+    wget            # 网络下载工具
+    tmux            # 终端多路复用工具
+    jq              # json美化过滤
+    yq              # yaml美化过滤
+    fastfetch       # 系统信息查看工具
 
     #! 命令行替代工具
     eza         # 替代ls
