5528b34a433490041b912f2f1f9558f3a826e3f3
thinkgo <thinkgo@aliyun.com>
2025-12-10 22:06:29 +0800

    fix: fix hyprland

--- home/graphical/windows/hyprland.nix
+++ home/graphical/windows/hyprland.nix
@@ -1,40 +1,44 @@
 { config, lib, pkgs, ... }: 
 {
   home.sessionVariables = {
-    NIXOS_OZONE_WL = 1;
-    __GL_GSYNC_ALLOWED = 0;
-    __GL_VRR_ALLOWED = 0;
-    SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
-    DISABLE_QT5_COMPAT = 0;
-    GDK_BACKEND = "wayland";
-    ANKI_WAYLAND = 1;
-    DIRENV_LOG_FORMAT = "";
-    WLR_DRM_NO_ATOMIC = 1;
-    QT_AUTO_SCREEN_SCALE_FACTOR = 1;
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
-    QT_QPA_PLATFORM = "wayland";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_STYLE_OVERRIDE = "kvantum";
-    MOZ_ENABLE_WAYLAND = 1;
-    WLR_BACKEND = "vulkan";
-    WLR_RENDERER = "vulkan";
-    WLR_NO_HARDWARE_CURSORS = 1;
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    GTK_THEME = "Colloid-Green-Dark-Gruvbox";
-    GRIMBLAST_HIDE_CURSOR = 0;
+    # NIXOS_OZONE_WL = 1;
+    # __GL_GSYNC_ALLOWED = 0;
+    # __GL_VRR_ALLOWED = 0;
+    # SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
+    # DISABLE_QT5_COMPAT = 0;
+    # GDK_BACKEND = "wayland";
+    # ANKI_WAYLAND = 1;
+    # DIRENV_LOG_FORMAT = "";
+    # WLR_DRM_NO_ATOMIC = 1;
+    # QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+    # QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+    # QT_QPA_PLATFORM = "wayland";
+    # QT_QPA_PLATFORMTHEME = "qt5ct";
+    # QT_STYLE_OVERRIDE = "kvantum";
+    # MOZ_ENABLE_WAYLAND = 1;
+    # WLR_BACKEND = "vulkan";
+    # WLR_RENDERER = "vulkan";
+    # WLR_NO_HARDWARE_CURSORS = 1;
+    # XDG_CURRENT_DESKTOP = "Hyprland";
+    # XDG_SESSION_TYPE = "wayland";
+    # XDG_SESSION_DESKTOP = "Hyprland";
+    # SDL_VIDEODRIVER = "wayland";
+    # CLUTTER_BACKEND = "wayland";
+    # GTK_THEME = "Colloid-Green-Dark-Gruvbox";
+    # GRIMBLAST_HIDE_CURSOR = 0;
   };
 
   home.packages = with pkgs;[
     hyprland
+    hyprpaper
+    hyprpicker
+    # hyprlock
+    # hypridle
     wayland
     swww
-    grimblast
-    hyprpicker
+    waybar
     grim
+    grimblast
     slurp
     wl-clip-persist
     cliphist
@@ -46,33 +50,29 @@
     enable = true;
     plugins = [];
     settings =  {
-      "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-              let ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
+      # "$mod" = "SUPER";
+      # bind =
+      #   [
+      #     "$mod, F, exec, firefox"
+      #     ", Print, exec, grimblast copy area"
+      #   ]
+      #   ++ (
+      #     # workspaces
+      #     # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+      #     builtins.concatLists (builtins.genList (i:
+      #         let ws = i + 1;
+      #         in [
+      #           "$mod, code:1${toString i}, workspace, ${toString ws}"
+      #           "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+      #         ]
+      #       )
+      #       9)
+      #   );
     };
 
-    xwayland = {
-      enable = true;
-      # hidpi = true;
-    };
-    # enableNvidiaPatches = false;
     systemd.enable = true;
     systemd.variables = ["--all"];
+    xwayland.enable = true;
   };
   programs.kitty.enable = true;
 }
