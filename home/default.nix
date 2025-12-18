a11f407bee9b0db2a80ce3b2484c904ced14e273
thinkgo <thinkgo@aliyun.com>
2025-12-18 19:34:32 +0800

    feat: add programs

--- home/default.nix
+++ home/default.nix
@@ -17,6 +17,7 @@
   imports = [
     ./systemd.nix
     ./fonts.nix
+    ./programs.nix
     ./terminal
   ] ++ lib.optionals customize.graphics [
     ./graphical
@@ -35,38 +36,9 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs;[
-    #! 基础工具
-    tree            # 目录树
-    nmap            # 网络扫描工具
-    netcat-openbsd  # nc 网络工具
-    curl            # 网络请求工具
-    wget            # 网络下载工具
-    tmux            # 终端多路复用工具
-    jq              # json美化过滤
-    jaq             # json美化过滤
-    yq              # yaml美化过滤
-    fastfetch       # 系统信息查看工具
-
-    #! 命令行替代工具
-    eza         # 替代ls
-    ripgrep     # 替代grep
-    fd          # 替代find
-    dust        # 替代du  
-    bat         # 替代cat
-
-    #! 通用工具
-    tealdeer    # 快速查找命令, 同tldr
-    tokei       # 统计代码行数
-    glow        # markdown预览器
-    bottom      # 图形化进程/系统监控器
-    typos       # 检查拼写错误
-    mdbook      # 从markdown文档生成book
-    aria2       # 高速下载工具
-    trash-cli   # 回收站
-    parallel    # 并行执行命令
-
-    #! home-manager - terminal 统一管理终端应用
-    #! home-manager - graphical 统一管理图形化应用
+    #! usual 统一管理常用应用
+    #! terminal 统一管理终端应用
+    #! graphical 统一管理图形化应用
 
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
---
+++ home/programs.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+     #! 基础工具
+    tree            # 目录树
+    nmap            # 网络扫描工具
+    netcat-openbsd  # nc 网络工具
+    curl            # 网络请求工具
+    wget            # 网络下载工具
+    tmux            # 终端多路复用工具
+    jq              # json美化过滤
+    jaq             # json美化过滤
+    yq              # yaml美化过滤
+    fastfetch       # 系统信息查看工具
+
+    #! 命令行替代工具
+    eza         # 替代ls
+    ripgrep     # 替代grep
+    fd          # 替代find
+    dust        # 替代du  
+    bat         # 替代cat
+
+    #! 通用工具
+    tokei       # 统计代码行数
+    glow        # markdown预览器
+    bottom      # 图形化进程/系统监控器
+    typos       # 检查拼写错误
+    mdbook      # 从markdown文档生成book
+    aria2       # 高速下载工具
+    trash-cli   # 回收站
+    parallel    # 并行执行命令
+  ];
+
+  programs = {
+    # 快速查找命令, 同tldr
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.tealdeer.enable
+    tealdeer = { 
+      enable = true;
+      enableAutoUpdates = true;
+      # https://tealdeer-rs.github.io/tealdeer/config.html
+      settings = {
+        updates = {
+          auto_update = true;
+          auto_update_interval_hours = 72;
+        };
+      };
+    };
+  };
+}
