22b7e57317d4bc8825e9e181a6ff92055a6ae05e
thinkgo <thinkgo@aliyun.com>
2025-12-23 16:25:22 +0800

    fix: remove code

--- home/graphical/editor/default.nix
+++ home/graphical/editor/default.nix
@@ -1,12 +1,10 @@
 { config, lib, pkgs, ... }: 
 {
   imports = [
-    ./vscode.nix
     ./zed.nix
   ];
 
   home.packages = with pkgs;[
-    # vscode        # 代码编辑器
     # zed           # 代码编辑器
   ];
 }
