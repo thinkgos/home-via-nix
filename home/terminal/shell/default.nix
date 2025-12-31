f4622fa8a0d71dc34479d43463ee3c2c81ed9273
thinkgo <thinkgo@aliyun.com>
2025-12-31 15:08:40 +0800

    feat: add fzf more aliases

--- home/terminal/shell/default.nix
+++ home/terminal/shell/default.nix
@@ -44,6 +44,9 @@ in
     nv = "nvim";
     tf = "terraform";
     jq = "jaq";
+    fb = "fzf --preview 'bat --color=always {-1}'";
+    fbv = "fzf --preview 'bat --color=always {}' --bind 'enter:become(vim {-1})' --bind 'ctrl-o:execute:vim {-1}'";
+    frv = "(RELOAD='reload:rg --column --color=always --smart-case {q} || :' && fzf --disabled --ansi --bind \"start:$RELOAD\" --bind \"change:$RELOAD\" --bind 'enter:become:vim {1} +{2}' --bind 'ctrl-o:execute:vim {1} +{2}' --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~4,+{2}+4/3,<80(up)')";
   };
 
   programs = {
