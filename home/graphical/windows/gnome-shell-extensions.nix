f970aff70147f5d4ae4f7f12de78b15b27c96016
thinkgo <thinkgo@aliyun.com>
2026-01-07 08:50:07 +0800

    refactor: windows placholder

--- home/graphical/windows/gnome-shell-extensions.nix
+++ home/graphical/windows/gnome-shell-extensions.nix
@@ -115,9 +115,9 @@ in
         "code.desktop:1"
         "realvnc-vncviewer.desktop:3"
         "dev.zed.Zed.desktop:1"
-        "dev.warp.Warp.desktop:2"
-        "kitty.desktop:2"
-        # "org.wezfurlong.wezterm.desktop:2"
+        "dev.warp.Warp.desktop:4"
+        "kitty.desktop:4"
+        # "org.wezfurlong.wezterm.desktop:4"
       ];
     };
 
