a411c540b706f8954fb1e37b0534d822b52043df
thinkgo <thinkgo@aliyun.com>
2026-01-13 17:16:56 +0800

    feat: add nushell polars

--- home/terminal/shell/nushell.nix
+++ home/terminal/shell/nushell.nix
@@ -12,8 +12,14 @@
     };
     configFile.source = ./nushell/config.nu;
     envFile.source = ./nushell/env.nu;
-    plugins = [
-
+    plugins = with pkgs; [
+      nushellPlugins.gstat # Git status plugin for Nushell
+      nushellPlugins.polars # Polars plugin for Nushell
+      nushellPlugins.formats # Formats plugin for Nushell
+      nushellPlugins.query # Nushell plugin to query JSON, XML, and various web data
+      nushellPlugins.highlight # Syntax highlighting for Nushell
+      nushellPlugins.semver # Semantic versioning plugin for Nushell
+      # nushellPlugins.skim # Fuzzy finder for Nushell
     ];
   };
 }
