dc52be5874a8a6064feb38ecaf235e343c5108f6
thinkgo <thinkgo@aliyun.com>
2026-01-21 14:33:30 +0800

    refactor: only use offical plugin

--- home/terminal/shell/nushell.nix
+++ home/terminal/shell/nushell.nix
@@ -13,12 +13,12 @@
     configFile.source = ./nushell/config.nu;
     envFile.source = ./nushell/env.nu;
     plugins = with pkgs; [
-      nushellPlugins.gstat # Git status plugin for Nushell
       nushellPlugins.polars # Polars plugin for Nushell
+      nushellPlugins.gstat # Git status plugin for Nushell
       nushellPlugins.formats # Formats plugin for Nushell
       nushellPlugins.query # Nushell plugin to query JSON, XML, and various web data
-      nushellPlugins.highlight # Syntax highlighting for Nushell
-      nushellPlugins.semver # Semantic versioning plugin for Nushell
+      # nushellPlugins.highlight # Syntax highlighting for Nushell
+      # nushellPlugins.semver # Semantic versioning plugin for Nushell
       # nushellPlugins.skim # Fuzzy finder for Nushell
     ];
   };
