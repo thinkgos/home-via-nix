c2dd7d07df60ddac1f0617b485fab15e34053d1d
thinkgo <thinkgo@aliyun.com>
2026-04-14 17:31:59 +0800

    fix: fix font

--- home/fonts.nix
+++ home/fonts.nix
@@ -6,16 +6,22 @@
 }:
 {
   home.packages = with pkgs; [
-    adwaita-fonts
-    adwaita-icon-theme
+    # core fonts
+    inter
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
+    # adwaita-fonts
+    adwaita-fonts
+    adwaita-icon-theme
+    # nerd-fonts
     nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
     nerd-fonts.hack
     nerd-fonts.meslo-lg
-    nerd-fonts.fira-code
+
+    font-awesome
   ];
   fonts.fontconfig = {
     enable = true;
@@ -23,15 +29,16 @@
       emoji = [ "Noto Color Emoji" ];
       monospace = [
         "JetBrainsMono Nerd Font Mono"
-        "MesloLGL Nerd Font Mono"
         "FiraCode Nerd Font Mono"
+        "MesloLGL Nerd Font Mono"
         "Hack Nerd Font"
         "MesloLGS NF"
         "Noto Sans Mono CJK SC"
       ];
       sansSerif = [
+        "Inter"
         "Adwaita Sans"
-        "Noto Serif CJK SC"
+        "Noto Sans CJK SC"
       ];
       serif = [
         "Adwaita Sans"
