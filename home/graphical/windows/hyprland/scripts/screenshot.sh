f1d32eebc53c251e6f6f6f5ec1cf05c49c1d2ce8
thinkgo <thinkgo@aliyun.com>
2026-04-19 07:26:10 +0800

    fix: fix hypr scripts

--- home/graphical/windows/hyprland/scripts/screenshot.sh
+++ home/graphical/windows/hyprland/scripts/screenshot.sh
@@ -5,23 +5,32 @@ mkdir -p "${screenshotDir}"
 
 mode="$1"
 file="${screenshotDir}/$(date +'%Y%m%d%H%M%S').png"
+
+do_error() {
+    notify-send "截图取消" "操作已取消或发生错误" -i dialog-information
+    exit 1
+}
+
 case "$mode" in
 active)
-    hyprctl -j activewindow \
-    | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' \
-    | grim -g - "$file"
+    hyprctl -j activewindow |
+        jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' |
+        grim -g - "$file" || do_error
     ;;
 full)
-    grim "$file"
+    grim "$file" || do_error
     ;;
 region)
-    grim -g "$(slurp)" "$file"
+    region=$(slurp) || do_error
+    grim -g "$region" "$file" || do_error
     ;;
 annotate)
-    grim -g "$(slurp)" - | satty --filename -
+    region=$(slurp) || do_error
+    grim -g "$region" - | satty --filename - || do_error
     return
     ;;
 esac
 
+wl-copy -t image/png <"$file"
 notify-send "已获取截图" "你可以从剪贴板粘贴截图" -i camera-photo
-wl-copy -t image/png < "$file"
+exit 0
