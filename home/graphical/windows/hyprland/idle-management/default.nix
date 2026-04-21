44c5d546ed10e9a53b08f246c9a97ba3ee00edb0
thinkgo <thinkgo@aliyun.com>
2026-04-21 22:01:00 +0800

    fix: fix hyprland idle manager

--- home/graphical/windows/hyprland/idle-management/default.nix
+++ home/graphical/windows/hyprland/idle-management/default.nix
@@ -38,11 +38,12 @@
           on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
         }
-        {
-          timeout = 1800; # 30min
-          on-timeout = "systemctl suspend"; # suspend pc
-          # on-resume = ''notify-send " System Resumed!"'';
-        }
+        # 默认不挂起
+        # {
+        #   timeout = 1800; # 30min
+        #   on-timeout = "systemctl suspend"; # suspend pc
+        #   # on-resume = ''notify-send " System Resumed!"'';
+        # }
       ];
     };
   };
