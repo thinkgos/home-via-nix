0caf9a01d1a176cd35cf907dbc78a50ba7c3e490
thinkgo <thinkgo@aliyun.com>
2026-04-23 06:50:20 +0800

    refactor: refactor idle manger

--- home/graphical/windows/hyprland/idle-management/default.nix
+++ home/graphical/windows/hyprland/idle-management/default.nix
@@ -15,27 +15,28 @@
         # unlock_cmd = "loginctl unlock-session"; # kills hyprlock when unlocking (this is always run when "loginctl unlock-session" is called)
         before_sleep_cmd = "loginctl lock-session"; # ensures that the session is locked before going to sleep
         after_sleep_cmd = " hyprctl dispatch dpms on"; # turn of screen after sleep (not strictly necessary, but just in case)
+        # 是否忽略来自 Firefox/Steam 等应用的"禁止休眠"请求
         ignore_dbus_inhibit = false;
       };
       listener = [
         {
-          timeout = 300; # 5min.
-          # BUG: 键盘背光没法控制, 没有找到设备名.
-          on-timeout = "brightnessctl -s set 0"; # turn off keyboard backlight.
-          on-resume = "brightnessctl -r"; # turn on keyboard backlight.
-        }
-        {
-          timeout = 600; # 10min.
+          timeout = 300; # 5m
           on-timeout = ''notify-send " You are idle!"'';
           on-resume = ''notify-send " Welcome Back" " Enjoy !!!"'';
         }
         {
-          timeout = 900; # 15min
+          timeout = 600; # 10m
+          # BUG: 键盘背光没法控制, 没有找到设备名.
+          on-timeout = "brightnessctl -s set 10"; # turn down screen backlight.
+          on-resume = "brightnessctl -r"; # resume screen backlight.
+        }
+        {
+          timeout = 900; # 15m
           on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
           # on-resume = ''notify-send " System Unlocked!"'';
         }
         {
-          timeout = 1200; # 20min
+          timeout = 930; # 15m30s
           on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
           on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
         }
