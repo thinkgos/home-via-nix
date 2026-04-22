9a16f8258dea57788434c18193c383e68b2e862f
thinkgo <thinkgo@aliyun.com>
2026-04-22 10:58:58 +0800

    fix: fix hyprland windows rule

--- home/graphical/windows/hyprland/hypr-config/window.nix
+++ home/graphical/windows/hyprland/hypr-config/window.nix
@@ -14,8 +14,10 @@
     windowrule = [
       # 计算器/网络/蓝牙/音频
       "float on, size 750 480, move (monitor_w-750)*0.5 160, match:class ^(qalculate-gtk|nm-connection-editor|.blueman-manager-wrapped|org.pulseaudio.pavucontrol|org.fcitx.)$"
-      # 文件/监控
-      "float on, size monitor_w*0.6 monitor_h*0.6, center on, match:class ^(nemo|io.missioncenter.MissionCenter)$"
+      # 文件/邮件/监控/vlc/cc-switch/clash-verge
+      "float on, size monitor_w*0.6 monitor_h*0.6, center on, match:class ^(nemo|thunderbird|io.missioncenter.MissionCenter|vlc|cc-switch|clash-verge)$"
+      # LocalSend
+      "float on, size 720 800, move (monitor_w-720)*0.5 100, match:class ^(localsend_app)$"
       # 截图标注
       "float on, size monitor_w*0.8 monitor_h*0.8, center on, match:class ^(com.gabm.satty)$"
       # 下拉式终端窗口规则
