4e5dee2711373b0f5e9343be02e9e1104126ba5a
thinkgo <thinkgo@aliyun.com>
2026-04-23 15:51:10 +0800

    fix: fix gnome and hyprland session var

--- home/graphical/windows/gnome/default.nix
+++ home/graphical/windows/gnome/default.nix
@@ -10,8 +10,9 @@
     ./gnome-shell-extensions.nix
   ];
   home.sessionVariables = {
-    XDG_SESSION_DESKTOP = "GNOME";
-    XDG_CURRENT_DESKTOP = "GNOME";
+    NIXOS_OZONE_WL = 1; # Chromium 内核应用 使用 Wayland
+    MOZ_ENABLE_WAYLAND = 1; # Mozilla 系应用 使用 Wayland
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
   home.packages = with pkgs; [
     albert # 启动器(可选替代ulauncher)
