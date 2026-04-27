07e2fcb28738de9a9c6c1a04f517957f6330860f
thinkgo <thinkgo@aliyun.com>
2026-04-27 16:02:34 +0800

    refactor: use wayvnc

--- home/graphical/programs/vnc.nix
+++ home/graphical/programs/vnc.nix
@@ -6,16 +6,18 @@
 }:
 {
 
-  home.packages = [ pkgs.tigervnc ];
+  home.packages = [
+    # pkgs.tigervnc
+  ];
 
   # wayland vnc
   # BUG: Fetal error while capturing. Exiting...
-  # services.wayvnc = {
-  #   enable = true;
-  #   autoStart = true;
-  #   settings = {
-  #     address = "0.0.0.0";
-  #     port = 5900;
-  #   };
-  # };
+  services.wayvnc = {
+    enable = true;
+    autoStart = true;
+    settings = {
+      address = "0.0.0.0";
+      port = 5900;
+    };
+  };
 }
