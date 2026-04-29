e43752a28cf677a138018ec113f1f8ba94089772
thinkgo <thinkgo@aliyun.com>
2026-04-29 15:44:34 +0800

    fix: fix scripts

--- scripts/network_wait_online.sh
+++ scripts/network_wait_online.sh
@@ -13,4 +13,4 @@ EOF
 sudo systemctl daemon-reload
 echo "配置网络启动等待时间已生效！"
 echo "当前网络启动等待时间配置内容："
-systemctl cat "$NETWORKD_WAIT_ONLINE_CONF" | grep ExecStart
+cat "$NETWORKD_WAIT_ONLINE_CONF" | grep ExecStart
