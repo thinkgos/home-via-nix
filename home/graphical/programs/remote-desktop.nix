ad84fab50a20f21b8244b37b55876653c8868878
thinkgo <thinkgo@aliyun.com>
2026-04-30 16:16:39 +0800

    feat: add sunshine service

--- home/graphical/programs/remote-desktop.nix
+++ home/graphical/programs/remote-desktop.nix
@@ -25,4 +25,35 @@
       port = 5900;
     };
   };
+  # https://app.lizardbyte.dev/Sunshine/?lng=zh-CN
+  systemd.user.services = {
+    sunshine = lib.mkIf (customize.graphics.window == "hyprland") {
+      Unit = {
+        Description = "Sunshine Game Stream Server";
+        StartLimitIntervalSec = 500;
+        StartLimitBurst = 5;
+        After = [
+          "graphical-session.target"
+          "xdg-desktop-autostart.target"
+          "xdg-desktop-portal.service"
+        ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStartPre = "/bin/sleep 5"; # 延迟 5 秒启动
+
+        # 注意：这里的变量需要根据你实际的 Sunshine 路径或脚本进行替换
+        ExecStart = "${pkgs.sunshine}/bin/sunshine";
+        ExecStop = "pkill sunshine";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+        Alias = "sunshine.service";
+      };
+    };
+  };
 }
