3aa8d87f2b73ee078378d4413aa015ffed1a924d
thinkgo <thinkgo@aliyun.com>
2026-05-09 17:08:11 +0800

    fix: fix sunshine udev missing bug

--- scripts/general-settings.sh
+++ scripts/general-settings.sh
@@ -7,7 +7,11 @@ sudo update-locale LANG=zh_CN.UTF-8
 echo "设置时区..."
 sudo timedatectl set-timezone Asia/Shanghai
 
-echo "配置brightnessctl(自带udev规则)权限(注意: 重启后生效))..."
+# moonlight/sunshine的键盘/鼠标才能工作
+echo "配置sunshine udev规则(注意: 重启后生效)..."
+echo 'KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"' | sudo tee "/etc/udev/rules.d/85-sunshine.rules" >/dev/null
+
+echo "配置brightnessctl(自带udev规则)/sunshine权限(注意: 重启后生效))..."
 sudo usermod -aG video $USER
 sudo usermod -aG input $USER
 sudo usermod -aG render $USER
