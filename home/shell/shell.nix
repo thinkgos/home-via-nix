657f74ea42933006d67681f8a26409873556f3dc
thinkgo <thinkgo@aliyun.com>
2026-05-24 07:24:50 +0800

    refactor: refactor system

--- home/shell/shell.nix
+++ home/shell/shell.nix
@@ -10,24 +10,4 @@
     ./nushell/nushell.nix
     # ./bash.nix
   ];
-
-  home.shellAliases = {
-    # the top level attribute names.
-    # If you need to use a shell specific feature
-    # then make sure to use a shell specific option
-    cat = "bat -p";
-    nv = "nvim";
-    tf = "terraform";
-    trash = "trash -v";
-    cco = "claude";
-  };
-
-  # Create TMPDIR if it doesn't exist
-  home.activation.createTmpDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "$HOME/.cache/tmp"
-  '';
-
-  home.sessionPath = [
-    "$HOME/.local/bin"
-  ];
 }
--- home/system.nix
+++ home/system.nix
@@ -48,5 +48,23 @@
     BAT_PAGER = "less -r";
     TMPDIR = "$HOME/.cache/tmp";
   };
+  home.shellAliases = {
+    # the top level attribute names.
+    # If you need to use a shell specific feature
+    # then make sure to use a shell specific option
+    cat = "bat -p";
+    nv = "nvim";
+    tf = "terraform";
+    trash = "trash -v";
+    cco = "claude";
+  };
 
+  # Create TMPDIR if it doesn't exist
+  home.activation.createTmpDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/.cache/tmp"
+  '';
+
+  home.sessionPath = [
+    "$HOME/.local/bin"
+  ];
 }
