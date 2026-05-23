3e11037dfbc6e38f862d3cd39e93d1df021943cc
thinkgo <thinkgo@aliyun.com>
2026-05-23 15:16:52 +0800

    feat: add niri wme

---
+++ home/desktop/wm/wme/niri/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  power = {
+    lock = "/bin/notify-send -u critical '待实现'";
+    logout = "/bin/notify-send -u critical '注销有BUG(logout)!'";
+    shutdown = "/bin/systemctl poweroff";
+    reboot = "/bin/systemctl reboot";
+    # 休眠是写到swap文件(swap空间必须大于内存), 需要主板与BIOS/UEFI支持(主板必须支持ACPI S4状态)
+    # hibernate = "/bin/systemctl hibernate";
+    hibernate = "/bin/notify-send -u critical '系统暂不支持休眠(hibernate)!'";
+    suspend = "/bin/systemctl suspend";
+  };
+}
