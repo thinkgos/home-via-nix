ad8afaa80c68f6dc1e31aaaba32d8a855477c8ec
thinkgo <thinkgo@aliyun.com>
2026-06-11 16:31:18 +0800

    chore: split window

---
+++ hosts/thinkgo-work-niri.nix
@@ -0,0 +1,37 @@
+{ lib, pkgs }:
+lib.recursiveUpdate (import ./preset.nix { inherit lib pkgs; }) {
+  username = "thinkgo";
+  desktop = {
+    window = "niri";
+    window-version = "niri";
+    monitor-primary = "HDMI-A-1";
+    monitor-secondary = "DP-1";
+    # monitor-primary = "DP-1";
+    # monitor-secondary = "";
+  };
+  components = [
+    # 键鼠共享
+    # "deskflow"
+    # "lan-mouse"
+    "lan-mouse-client"
+  ];
+  apps = [
+    # "nushell"
+    # "yt-dlp"
+    # "wayvnc"
+    # "sunshine"
+    "obs-studio"
+    # 视频剪辑
+    # "aegisub"
+    # "subtitlecomposer"
+    # "subtitleedit"
+    # "ffsubsync"
+    # "python313Packages.subliminal"
+  ];
+  lang = [
+    "go"
+    "rust"
+    "python"
+    "cpp"
+  ];
+}
