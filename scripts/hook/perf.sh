716ae8922916b48ff632bb32d25b4f2c2885fe95
thinkgo <thinkgo@aliyun.com>
2026-06-24 21:53:01 +0800

    feat: add perf file limit

--- scripts/hook/perf.sh
+++ scripts/hook/perf.sh
@@ -5,19 +5,26 @@
 # 修改 /etc/default/grub 中的 GRUB_CMDLINE_LINUX_DEFAULT 参数增加以下 zswap 配置, 后执行 sudo update-grub2
 # zswap.enabled=1 zswap.compressor=zstd zswap.zpool=zsmalloc zswap.max_pool_percent=20
 
+sudo tee /etc/security/limits.d/99-max-files.conf >/dev/null <<'EOF'
+* soft nofile 1048576
+* hard nofile 1048576
+root soft nofile 1048576
+root hard nofile 1048576
+EOF
+
 # 调整 swappiness 为 20
 # 调整 vfs_cache_pressure 为 50
-sudo tee /etc/sysctl.d/99-swappiness.conf >/dev/null <<EOF
+sudo tee /etc/sysctl.d/99-swappiness.conf >/dev/null <<'EOF'
 vm.swappiness=20
 vm.vfs_cache_pressure=50
 EOF
 
 # 调整 core dump 放在 /var/crash 目录下
-sudo tee /etc/sysctl.d/99-core.conf >/dev/null <<EOF
+sudo tee /etc/sysctl.d/99-core.conf >/dev/null <<'EOF'
 kernel.core_pattern = /var/crash/core.%e.%p.%t
 EOF
 
-sudo tee /etc/sysctl.d/99-net.conf >/dev/null <<EOF
+sudo tee /etc/sysctl.d/99-net.conf >/dev/null <<'EOF'
 # 启用 FQ (Fair Queueing) 队列调度算法(BBR 强依赖此算法)
 net.core.default_qdisc = fq
 # 使用 BBR 拥塞控制算法
