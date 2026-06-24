5e5fd2f02fd369c5245cde0891346b7ca8833b6e
thinkgo <thinkgo@aliyun.com>
2026-06-24 17:02:06 +0800

    refactor: refactor terminal keybinding

--- home/desktop/terminal/kitty.nix
+++ home/desktop/terminal/kitty.nix
@@ -21,10 +21,21 @@
     # themeFile = "Monokai_Soda";
     # themeFile = "noirbuddy-oxide";
     keybindings = {
-      # font size
+      # 杂项
       "ctrl+-" = "change_font_size all -2.0"; # 减少字体大小
       "ctrl+=" = "change_font_size all +2.0"; # 增加字体大小
       "ctrl+0" = "change_font_size all 0"; # 重置字体大小
+      "ctrl+shift+c" = "copy_to_clipboard"; # 复制
+      "ctrl+shift+v" = "paste_from_clipboard"; # 粘贴
+      "ctrl+shift+n" = "new_os_window"; # 新建
+      "ctrl+shift+f" = "search_scrollback"; # 搜索
+      "ctrl+shift+p" = "command_palette"; # 命令面板
+      "ctrl+shift+," = "load_config_file"; # 重载配置
+      "home" = "scroll_home"; # 滚动到顶部
+      "end" = "scroll_end"; # 滚动到底部
+      "page_up" = "scroll_page_up"; # 向上滚动一页
+      "page_down" = "scroll_page_down"; # 向下滚动一页
+      "f11" = "toggle_fullscreen"; # 全屏
 
       # 标签页管理
       "ctrl+shift+t" = "new_tab"; # 新建标签页
@@ -33,6 +44,8 @@
       "ctrl+page_down" = "next_tab"; # 聚焦右边标签页
       "ctrl+shift+tab" = "prev_tab"; # 聚焦左边标签页
       "ctrl+tab" = "next_tab"; # 聚焦右边标签页
+      "ctrl+shift+page_up" = "move_tab_forward"; # 移动标签页到左边
+      "ctrl+shift+page_down" = "move_tab_backward"; # 移动标签页到右边
       "ctrl+1" = "goto_tab 1"; # 聚焦标签页1
       "ctrl+2" = "goto_tab 2"; # 聚焦标签页2
       "ctrl+3" = "goto_tab 3"; # 聚焦标签页3
@@ -47,11 +60,11 @@
       # 关闭窗口使用ctrl+d
       "ctrl+shift+e" = "launch --location=hsplit --cwd=current"; # 水平分割窗口
       "ctrl+shift+d" = "launch --location=vsplit --cwd=current"; # 垂直分割窗口
-      "ctrl+shift+[" = "previous_window"; # 上一个窗口
-      "ctrl+shift+left" = "previous_window"; # 上一个窗口
-      "ctrl+shift+]" = "next_window"; # 下一个窗口
-      "ctrl+shift+right" = "next_window"; # 下一个窗口
-      "ctrl+shift+r" = "start_resizing_window"; # 调整窗口大小
+      "ctrl+alt+up" = "previous_window"; # 聚焦上方窗格
+      "ctrl+alt+down" = "next_window"; # 聚焦下方窗格
+      "ctrl+alt+left" = "previous_window"; # 聚焦左方窗格
+      "ctrl+alt+right" = "next_window"; # 聚焦右方窗格
+      "ctrl+shift+z" = "start_resizing_window"; # 调整窗口大小
 
       # 布局
       "f1" = "toggle_layout stack"; # 临时切换到堆栈布局
