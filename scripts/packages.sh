9e8370aa6489b02eb97b6891afd0f44ec1a54966
thinkgo <thinkgo@aliyun.com>
2026-07-16 22:08:18 +0800

    fix: add seahorse

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,17 +4,17 @@ source $(cd "$(dirname "${BASH_SOURCE[0]}")/lib/" && pwd)/log4sh.sh
 
 log::info "安装必需的系统软件..."
 sudo apt install -y \
-    pipewire pipewire-pulse pipewire-alsa wireplumber pipewire-audio-client-libraries libspa-0.2-bluetooth libepoxy-dev \
-    brightnessctl bluez bluez-tools bluez-cups playerctl \
-    qt6-wayland \
-    fonts-noto-core fonts-noto-cjk \
-    gvfs \
-    network-manager \
-    libnotify-bin \
-    thermald \
-    power-profiles-daemon \
-    gnome-keyring \
-    language-pack-zh-hans
+	pipewire pipewire-pulse pipewire-alsa wireplumber pipewire-audio-client-libraries libspa-0.2-bluetooth libepoxy-dev \
+	brightnessctl bluez bluez-tools bluez-cups playerctl \
+	qt6-wayland \
+	fonts-noto-core fonts-noto-cjk \
+	gvfs \
+	network-manager \
+	libnotify-bin \
+	thermald \
+	power-profiles-daemon \
+	gnome-keyring seahorse \
+	language-pack-zh-hans
 
 sudo apt install -y \
-    gvfs-backends gvfs-fuse smbclient cifs-utils
+	gvfs-backends gvfs-fuse smbclient cifs-utils
