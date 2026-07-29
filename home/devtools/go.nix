aa2ce56d304566cbeb438d4ddb2bbf021c0bbf35
thinkgo <thinkgo@aliyun.com>
2026-07-29 08:33:35 +0800

    fix: fix go

--- home/devtools/go.nix
+++ home/devtools/go.nix
@@ -4,20 +4,9 @@
   pkgs,
   ...
 }:
-# gotools,gopls的modernize冲突
-# https://github.com/NixOS/nixpkgs/issues/509480
-let
-  gotoolsWithoutModernize = pkgs.symlinkJoin {
-    name = "gotools-without-modernize";
-    paths = [ pkgs.gotools ];
-    postBuild = ''
-      rm -f "$out/bin/modernize"
-    '';
-  };
-in
 {
   home.packages = with pkgs; [
-    gotoolsWithoutModernize # Additional tools for Go development
+    gotools # Additional tools for Go development
     gotests # Generate Go tests from your source code
     impl # Generate method stubs for implementing an interface
     gopls # Official language server for the Go language
