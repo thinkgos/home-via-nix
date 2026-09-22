a3b14073d80c60b6517ed3e0abe81a513f4d527a
thinkgo <thinkgo@aliyun.com>
2026-09-22 14:53:01 +0800

    fix: fix codex

--- home/ai/codex/default.nix
+++ home/ai/codex/default.nix
@@ -5,12 +5,15 @@
   ...
 }:
 {
+  # https://code.claude.com/docs/zh-CN/settings
   # https://nix-community.github.io/home-manager/options/home-manager/programs/codex.html
   programs.codex = {
     enable = true;
-    enableMcpIntegration = true;
+    enableMcpIntegration = false;
     marketplaces = import ./marketplaces.nix { inherit config lib pkgs; };
     plugins = import ./plugins.nix { inherit config lib pkgs; };
+    #  https://github.com/openai/codex/blob/main/codex-rs/config.md
+    settings = null; # import ./settings.nix { inherit config lib pkgs; };
     # 全局上下文
     # context = ""; # CODEX_HOME/AGENTS.md
     # contextOverride = null; # CODEX_HOME/AGENTS.override.md
@@ -18,7 +21,5 @@
     profiles = import ./profiles.nix { inherit config lib pkgs; };
     rules = import ./rules.nix { inherit config lib pkgs; };
     skills = import ./skills.nix { inherit config lib pkgs; };
-    #  https://github.com/openai/codex/blob/main/codex-rs/config.md
-    settings = import ./settings.nix { inherit config lib pkgs; };
   };
 }
--- home/ai/codex/profiles.nix
+++ home/ai/codex/profiles.nix
@@ -5,5 +5,14 @@
   ...
 }:
 {
-
+  xiaomi = {
+    model = "mimo-v2.5-pro";
+    model_reasoning_effort = "high";
+    disable_response_storage = true;
+    approval_policy = "on-request";
+    sandbox_mode = "workspace-write";
+    base_url = "https://api.xiaomimimo.com/v1";
+    wire_api = "responses";
+    requires_openai_auth = false;
+  };
 }
